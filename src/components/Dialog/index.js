6008d519e62d4f1fa581e6a000e0334b6a96ca79
OttoSouza <ottos.developer@gmail.com>
2020-05-01 20:58:42 -0300

    feat: add some icons

--- src/components/Dialog/index.js
+++ src/components/Dialog/index.js
@@ -1,39 +1,58 @@
-import React, { useState } from 'react';
-import Button from '@material-ui/core/Button';
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
 
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
-import { IconButton, Paper, Typography, } from '@material-ui/core';
-import DialogStyles from './styles';
-
-
+import { IconButton, Paper, Typography } from "@material-ui/core";
+import {
+  GiFallingLeaf,
+  GiHops,
+  GiCutDiamond,
+  GiArrowDunk,
+  GiPriceTag,
+  GiBeerStein,
+  GiTransparentSlime,
+} from "react-icons/gi";
+import { TiPlus, TiArrowForward } from "react-icons/ti";
+import DialogStyles from "./styles";
 
 export default function Details({ beer }) {
   const classes = DialogStyles();
   const [open, setOpen] = useState(false);
-  const [scroll, setScroll] = React.useState('paper');
+  const [scroll, setScroll] = React.useState("paper");
 
   const malts = beer.ingredients.malt.map((malt, index) => (
-
     <div key={index} className={classes.ingredientsContainer}>
-      <Typography className={classes.subtitles}><strong>Name</strong> {malt.name}</Typography>
-      <Typography className={classes.subtitles}><strong>Amount</strong> {malt.amount.value} {malt.amount.unit}</Typography>
+      <Typography className={classes.subtitles}>
+        <GiFallingLeaf /> {malt.name}
+      </Typography>
+      <Typography className={classes.subtitles}>
+        <GiCutDiamond /> {malt.amount.value} {malt.amount.unit}
+      </Typography>
     </div>
-
-  ))
+  ));
 
   const hops = beer.ingredients.hops.map((hops, index) => (
     <div key={index} className={classes.ingredientsContainer}>
-      <Typography className={classes.subtitles}><strong>Name</strong> {hops.name}</Typography>
-      <Typography className={classes.subtitles}><strong>Amount</strong> {`${hops.amount.value} ${hops.amount.unit}`}</Typography>
-      <Typography className={classes.subtitles}><strong>Add</strong> {hops.add} </Typography>
-      <Typography className={classes.subtitles}><strong>Attribute</strong> {hops.attribute} </Typography>
+      <Typography className={classes.subtitles}>
+        <GiHops /> {hops.name}
+      </Typography>
+      <Typography className={classes.subtitles}>
+        <GiCutDiamond /> {`${hops.amount.value} ${hops.amount.unit}`}
+      </Typography>
+      <Typography className={classes.subtitles}>
+        <TiPlus />
+        {hops.add}{" "}
+      </Typography>
+      <Typography className={classes.subtitles}>
+        <GiArrowDunk /> {hops.attribute}{" "}
+      </Typography>
     </div>
-  ))
+  ));
 
   const handleClickOpen = (scrollType) => () => {
     setOpen(true);
@@ -44,36 +63,48 @@ export default function Details({ beer }) {
     setOpen(false);
   };
 
-
-
   return (
     <>
-      <IconButton color="inherit" onClick={handleClickOpen('paper')}>
+      <IconButton color="inherit" onClick={handleClickOpen("paper")}>
         <MoreHorizIcon style={{ color: "#e02041" }} />
       </IconButton>
       <Dialog open={open} onClose={handleClose} scroll={scroll}>
-        <DialogTitle style={{ textAlign: 'center' }}><img className={classes.img} src={beer.image_url} alt={beer.name} /></DialogTitle>
+        <DialogTitle style={{ textAlign: "center" }}>
+          <img className={classes.img} src={beer.image_url} alt={beer.name} />
+        </DialogTitle>
         <DialogContent>
-          <DialogContentText style={{ textAlign: 'justify' }}>
+          <DialogContentText style={{ textAlign: "justify" }}>
             {beer.description}
           </DialogContentText>
-          <Paper className={classes.paperContainer} >
-
-            <Typography className={classes.title}>Basic Informations</Typography>
-            <Typography className={classes.subtitles}><strong>Name</strong> {beer.name}</Typography>
-            <Typography className={classes.subtitles}><strong>TagLine</strong> {beer.tagline}</Typography>
-            <Typography className={classes.subtitles}><strong>First Brewed</strong> {beer.first_brewed}</Typography>
+          <Paper className={classes.paperContainer}>
+            <Typography className={classes.title}>
+              Basic Informations
+            </Typography>
+            <Typography className={classes.subtitles}>
+              <TiArrowForward /> {beer.name}
+            </Typography>
+            <Typography className={classes.subtitles}>
+              <GiPriceTag /> {beer.tagline}
+            </Typography>
+            <Typography className={classes.subtitles}>
+              <GiBeerStein /> {beer.first_brewed}
+            </Typography>
             <Typography className={classes.title}>Malts</Typography>
             {malts}
             <Typography className={classes.title}>Holps</Typography>
             {hops}
             <Typography className={classes.title}>Yeast</Typography>
-            <Typography className={classes.subtitles}><strong>Name</strong> {beer.ingredients.yeast}</Typography>
+            <Typography className={classes.subtitles}>
+              <GiTransparentSlime /> {beer.ingredients.yeast}
+            </Typography>
           </Paper>
-          
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} className={classes.button} variant='contained'>
+          <Button
+            onClick={handleClose}
+            className={classes.button}
+            variant="contained"
+          >
             Cancel
           </Button>
         </DialogActions>
--- src/components/Dialog/styles.js
+++ src/components/Dialog/styles.js
@@ -2,39 +2,38 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const DialogStyles = makeStyles((theme) => ({
   img: {
-    height: '25vh',
-    width: '25wh',
+    height: "25vh",
+    width: "25wh",
   },
 
   paperContainer: {
-    padding: '5px',
-    width: '100%'
+    padding: "5px",
+    maxWidth: "100%",
   },
 
   title: {
-    fontSize: '20px',
-    fontWeight: 'bold',
-    margin: '0 0 5px 10px',
-    paddingBottom: '5px',
+    fontSize: "20px",
+    fontWeight: "bold",
+    margin: "0 0 5px 10px",
+    paddingBottom: "5px",
     borderBottom: `2px solid ${theme.palette.divider}`,
-    color: '#650000',
+    color: "#650000",
   },
 
   subtitles: {
-    fontSize: '14px',
-    margin: '0 0 5px 20px',
-    paddingBottom: '5px',
+    fontSize: "14px",
+    margin: "0 0 5px 20px",
+    paddingBottom: "5px",
   },
 
   ingredientsContainer: {
-    display: 'flex',
-
+    display: "flex",
   },
-  button: {
-    color: '#fff',
-    background: '#e02041'
 
-  }
+  button: {
+    color: "#fff",
+    background: "#e02041",
+  },
 }));
 
 export default DialogStyles;
